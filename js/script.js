bdb5c9a138d2bbd8b81f52ee5c08c38c9edc5fa6
Justine Derrick <270139408@bun2-206-17.acg.ac.nz>
2021-11-23 16:42:46 +1300

    changes to js, map markers, appending cards

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ console.log("howdy");
 // define map key 
 let script = "<script src='https://maps.googleapis.com/maps/api/js?key=" + key + "&libraries=places&callback=initMap' async defer >";
 
+// document query selectors
+const applyFiltersBtn = document.querySelector("#applyFiltersBtn");
+
+
+// --------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------
+
 
 
 // append map key to body
@@ -12,28 +19,8 @@ $(document).ready(function(){
 });
 
 
-// initialise map
-
-let map;
-let markers = [];
-
-function initMap(){
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 11, 
-        center: {lat:-36.7667, lng:175.5000},
-    });
-}
-
-
-
-
-
-
-
-
-
-
-
+// --------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------
 
 
 // object array - accomodation details
@@ -45,7 +32,10 @@ let accommodation = [
         name: "The Carlton Hotel",
         type: "Hotel", 
         location: "Whitianga, Coromandel, NZ",
+        latitude: -36.83898421622976, 
+        longitude: 175.69179517137826,
         rating: 4.7,
+        reviewsNo: 84,
         costPerNight: 157,
         minGuest: 1,
         maxGuest: 2,
@@ -53,7 +43,7 @@ let accommodation = [
         maxNight: 5,
         descriptionShort: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dolore explicabo rem iste et perferendis itaque autem aut a inventore nam, temporibus ab quasi voluptate dignissimos error qui nulla sequi?",
         ammenities: ["Free Wifi", "Gym Facilities", "Late Check Out", "Free Parking"],
-        topStay: true,
+
     },
 
     {
@@ -62,7 +52,10 @@ let accommodation = [
         name: "Morland Hotel",
         type: "Hotel", 
         location: "Opito Bay, Coromandel, NZ",
+        latitude: -36.71498397906826,
+        longitude: 175.7945025259624,
         rating: 4.5,
+        reviewsNo: 123,
         costPerNight: 157,
         minGuest: 1,
         maxGuest: 2,
@@ -70,8 +63,7 @@ let accommodation = [
         maxNight: 5,
         descriptionShort: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dolore explicabo rem iste et perferendis itaque autem aut a inventore nam, temporibus ab quasi voluptate dignissimos error qui nulla sequi?",
         ammenities: ["Free Wifi", "Late Check Out"],
-        topStay: false,
-        
+                
     },
 
     {
@@ -80,7 +72,10 @@ let accommodation = [
         name: "The Peninsula Hotel",
         type: "Hotel", 
         location: "Matarangi, Coromandel, NZ",
+        latitude: -36.73275387156286,
+        longitude:  175.6446552829269, 
         rating: 4.2,
+        reviewsNo: 72,
         costPerNight: 157,
         minGuest: 1,
         maxGuest: 2,
@@ -88,7 +83,7 @@ let accommodation = [
         maxNight: 5,
         descriptionShort: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dolore explicabo rem iste et perferendis itaque autem aut a inventore nam, temporibus ab quasi voluptate dignissimos error qui nulla sequi?",
         ammenities: ["Free Wifi", "Late Check Out"],
-        topStay: false,
+       
     },
 
     {
@@ -97,7 +92,10 @@ let accommodation = [
         name: "The Cathedral",
         type: "Hotel", 
         location: "Hahei, Coromandel, NZ",
+        latitude: -36.84091593931837, 
+        longitude: 175.80844929618638, 
         rating: 4.9,
+        reviewsNo: 200,
         costPerNight: 157,
         minGuest: 1,
         maxGuest: 2,
@@ -105,7 +103,7 @@ let accommodation = [
         maxNight: 5,
         descriptionShort: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dolore explicabo rem iste et perferendis itaque autem aut a inventore nam, temporibus ab quasi voluptate dignissimos error qui nulla sequi?",
         ammenities: ["Free Wifi", "Late Check Out"],
-        topStay: true,
+       
     },
 
     {
@@ -114,7 +112,10 @@ let accommodation = [
         name: "The Ocean View Hotel",
         type: "Hotel", 
         location: "Whitianga, Coromandel, NZ",
+        latitude: -36.82994337395195,
+        longitude: 175.7049525979734, 
         rating: 3.5,
+        reviewsNo: 64,
         costPerNight: 157,
         minGuest: 1,
         maxGuest: 2,
@@ -122,7 +123,7 @@ let accommodation = [
         maxNight: 5,
         descriptionShort: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dolore explicabo rem iste et perferendis itaque autem aut a inventore nam, temporibus ab quasi voluptate dignissimos error qui nulla sequi?",
         ammenities: ["Free Wifi", "Late Check Out"],
-        topStay: false,
+        
     },
 
     {
@@ -131,7 +132,10 @@ let accommodation = [
         name: "Seaview Motel",
         type: "Motel", 
         location: "Whitianga, Coromandel, NZ",
+        latitude: -36.82515425566253, 
+        longitude: 175.70227305787188,
         rating: 3.7,
+        reviewsNo: 157,
         costPerNight: 90,
         minGuest: 2,
         maxGuest: 4,
@@ -139,8 +143,7 @@ let accommodation = [
         maxNight: 10,
         descriptionShort: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dolore explicabo rem iste et perferendis itaque autem aut a inventore nam, temporibus ab quasi voluptate dignissimos error qui nulla sequi?",
         ammenities: ["Pool", "Free Cancelation","Free Parking"],
-        topStay: false,
-
+       
     },
 
     {
@@ -149,16 +152,18 @@ let accommodation = [
         name: "Cook's Beach Motel",
         type: "Motel", 
         location: "Cook's Beach, Coromandel, NZ",
+        latitude: -36.83763932778573, 
+        longitude: 175.74035469061513,
         rating: 4.1,
+        reviewsNo: 52,
         costPerNight: 90,
         minGuest: 2,
         maxGuest: 4,
         minNight: 3,
         maxNight: 10,
-        descriptionShort: "",
+        descriptionShort: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dolore explicabo rem iste et perferendis itaque autem aut a inventore nam, temporibus ab quasi voluptate dignissimos error qui nulla sequi?",
         ammenities: ["Late Check Out"],
-        topStay: false,
-
+        
     },
 
     {
@@ -167,7 +172,10 @@ let accommodation = [
         name: "Coromandel Youth Hostel",
         type: "Hostel", 
         location: "Whitianga, Coromandel, NZ",
+        latitude: -36.83949375865749,
+        longitude: 175.69138491931892,
         rating: 3.9,
+        reviewsNo: 313,
         costPerNight: 30,
         minGuest: 1,
         maxGuest: 1,
@@ -175,7 +183,7 @@ let accommodation = [
         maxNight: 10,
         descriptionShort: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dolore explicabo rem iste et perferendis itaque autem aut a inventore nam, temporibus ab quasi voluptate dignissimos error qui nulla sequi?",
         ammenities: ["Free Wifi", "Free Cancelation"],
-        topStay: false,
+        
     },
 
     {
@@ -184,7 +192,10 @@ let accommodation = [
         name: "Optio Bay Backpackers",
         type: "Hostel", 
         location: "Opito Bay, Coromandel, NZ",
+        latitude: -36.72296194864805, 
+        longitude: 175.80564070167569,
         rating: 4.3,
+        reviewsNo: 202,
         costPerNight: 30,
         minGuest: 1,
         maxGuest: 1,
@@ -192,8 +203,7 @@ let accommodation = [
         maxNight: 10,
         descriptionShort: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dolore explicabo rem iste et perferendis itaque autem aut a inventore nam, temporibus ab quasi voluptate dignissimos error qui nulla sequi?",
         ammenities: ["Free Wifi", "Free Cancelation"],
-        topStay: true,
-
+      
     },
 
     {
@@ -202,7 +212,10 @@ let accommodation = [
         name: "Relaxation on the Cove",
         type: "House", 
         location: "Hahei, Coromandel, NZ",
+        latitude: -36.8435427107329, 
+        longitude: 175.8018675620926,
         rating: 4.6,
+        reviewsNo: 322,
         costPerNight: 240,
         minGuest: 1,
         maxGuest: 4,
@@ -210,8 +223,7 @@ let accommodation = [
         maxNight: 15,
         descriptionShort: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dolore explicabo rem iste et perferendis itaque autem aut a inventore nam, temporibus ab quasi voluptate dignissimos error qui nulla sequi?",
         ammenities: ["Free Wifi", "Late Check Out", "Free Parking"],
-        topStay: true,
-
+       
     },
 
     {
@@ -220,7 +232,10 @@ let accommodation = [
         name: "Seaside Stay",
         type: "House", 
         location: "Matarangi, Coromandel, NZ",
+        latitude: -36.843780488706656, 
+        longitude: 175.8148605326123,
         rating: 4.6,
+        reviewsNo: 79,
         costPerNight: 240,
         minGuest: 1,
         maxGuest: 4,
@@ -228,7 +243,7 @@ let accommodation = [
         maxNight: 15,
         descriptionShort: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dolore explicabo rem iste et perferendis itaque autem aut a inventore nam, temporibus ab quasi voluptate dignissimos error qui nulla sequi?",
         ammenities: ["Free Wifi", "Late Check Out", "Free Parking", "Spa"],
-        topStay: true,
+       
     },
 
 ];
@@ -236,63 +251,99 @@ let accommodation = [
 console.log(accommodation);
 
 
-// click to show filters
+// --------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------
 
-function showFilters(){
-    let show = document.getElementById("searchFilters");
 
-    show.style.display = "block";
+// initialise map
+
+let map;
+let markers = [];
+
+function initMap(){
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12, 
+        center: {lat:-36.7667, lng:175.5000},
+    });
+}
+
+
+// initial filtering options
+
+function filterOptions(event){
+    event.preventDefault();
+    console.log("clicked");
+
+    let msDay = 100 * 36000 * 24;
+
+    let checkInDate = new Date($("#checkInDate").val());
+    let checkOutDate = new Date($("#checkOutDate").val());
+    console.log(checkInDate);
+    console.log(checkOutDate);
+
+    let dateDifference = checkOutDate.getTime() - checkInDate.getTime();
+    let numberOfDays = dateDifference/msDay;
+    console.log(numberOfDays);
+
+    let numberOfPeople = $("#guestsNumber").val();
+    console.log(numberOfPeople);
+
+    displayOptions(numberOfDays, numberOfPeople);
 
 }
 
-$("#locationSearch").click(function(){
-    showFilters();
-});
 
-function hideFilters(){
-    let show = document.getElementById("whereTo");
+// dsiplaying options
 
-    show.style.display = "none";
-}
+function displayOptions(nights, guests){
 
-function showSummary(){
-    let show = document.getElementById("searchSummary");
+    reloadMarkers();
 
-    show.style.display = "block";
-}
+    console.log(nights);
+    console.log(guests);
 
-$("#applyFiltersBtn").click(function(){
-    hideFilters();
-    showSummary();
-});
+    $("#cardContainerSearchResults").empty();
+    $("#cardContainerTopStay").empty();
 
-
-
-// create top stay cards
-function topStays(){
-    for(let i = 0; i < accommodation.length; i++){
-        if(accommodation[i].topStay === true){
-            generateTopCard(i);
-        }
-    }
-}
-
-topStays();
-
-
-
-// create results cards
-
-function searchResults(){
-    for(let i = 0; i < accommodation.length; i++){
-        if(accommodation[i].topStay === true){
+    for (let i=0; i < accommodation.length; i++){
+        if ((nights >= accommodation[i].minNight && nights <= accommodation[i].maxNight) && (guests >= accommodation[i].minGuest && guests <= accommodation[i].maxGuest)){
             generateResultCard(i);
-        }
+
+            let location = {lat: accommodation[i].latitude, lng: accommodation[i].longitude};
+            console.log(location);
+            
+            let marker = new google.maps.Marker({
+                position: location,
+                map:map,
+            });
+
+            // console.log(marker);
+            markers.push(marker);
+
+        } 
+        
+        if ((nights >= accommodation[i].minNight && nights <= accommodation[i].maxNight) && (guests >= accommodation[i].minGuest && guests <= accommodation[i].maxGuest) && (accommodation[i].rating >= 4)){
+            generateTopCard(i);
+        } 
     }
 }
 
-searchResults();
+console.log(markers);
 
+function reloadMarkers() {
+
+    for (let i=0; i < markers.length; i++){
+        markers[i].setMap(null);
+    };
+
+    markers = [];
+
+}
+
+
+// -------------------------
+
+// card generators
 
 function generateTopCard(x){
     $("#cardContainerTopStay").append(
@@ -317,8 +368,14 @@ function generateResultCard(x){
         <div class="card">
             <img class="card-img-top" src="${accommodation[x].mainImage}" alt="Card image cap">
             <div class="card-body">
+            <div class="card-top-line">
                 <h5 class="card-title">${accommodation[x].name}</h5>
-                <h6 class="card-location"><i>${accommodation[x].location}</i></h5>
+            </div>
+                <h6 class="card-location">${accommodation[x].location}</h5>
+                <div class="reviews">
+                    <p class="rating-text"><i class="fas fa-star"></i>  ${accommodation[x].rating}</p> <p class="review-number"><i>(${accommodation[x].reviewsNo} reviews)</i></p>
+                </div>
+                <p class="price-text">$${accommodation[x].costPerNight}/night</p>
                 <p class="card-text">${accommodation[x].descriptionShort}</p>
                 <a href="#" class="card-link"><u>More Info</u>  <i class="fas fa-angle-right"></i></a>
             </div>
@@ -326,3 +383,67 @@ function generateResultCard(x){
         `
     );
 }
+
+
+// event listeners
+
+applyFiltersBtn.addEventListener("click", filterOptions);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// click to show filters
+
+// function showFilters(){
+//     let show = document.getElementById("searchFilters");
+
+//     show.style.display = "block";
+
+// }
+
+// $("#locationSearch").click(function(){
+//     showFilters();
+// });
+
+// function hideFilters(){
+//     let show = document.getElementById("whereTo");
+
+//     show.style.display = "none";
+// }
+
+// function showSummary(){
+//     let show = document.getElementById("searchSummary");
+
+//     show.style.display = "block";
+// }
+
+// $("#applyFiltersBtn").click(function(){
+//     hideFilters();
+//     showSummary();
+// });
+
+
+// create top stay cards
+// function topStays(){
+//     for(let i = 0; i < accommodation.length; i++){
+//         if(accommodation[i].rating >= 4.5){
+//             generateTopCard(i);
+//         }
+//     }
+// }
+
+// topStays();
+
+
+
